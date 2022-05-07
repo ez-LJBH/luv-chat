afc084bef0bd0b681fe1e25fc25676a5e1c3708a
BoSong Choi <index.cbsx@gmail.com>
2022-05-07 22:30:38 +0900

    갤러리 구조 생성 및 데이터 설정

---
+++ src/components/GallerySection/index.jsx
@@ -0,0 +1,9 @@
+import * as Styled from "./styled";
+
+import React from "react";
+
+const GallerySection = () => {
+  return <Styled.Container>GallerySection</Styled.Container>;
+};
+
+export default GallerySection;
--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -1,7 +1,14 @@
+import * as Styled from "./styled";
+
+import GallerySection from "../../components/GallerySection";
 import React from "react";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <Styled.Container>
+      <GallerySection />
+    </Styled.Container>
+  );
 };
 
 export default Gallery;
