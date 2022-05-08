6f26bef2a7d0b30b7b0f9fe156a23ea8bdecb07c
juillet <salut.juilllet@gmail.com>
2022-05-08 14:33:08 +0900

    Main 틀

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import * as Styled from "./styled";
+import { FiCamera } from 'react-icons/fi';
 
 const Main = () => {
-  return <div>Main</div>;
+  const loveDate = new Date('2022-05-04');
+  const loveYear = loveDate.getFullYear();
+  const loveMonth = loveDate.getMonth();
+  const loveDay = loveDate.getDate();
+
+  const nowDate = new Date();
+
+  const passedTime = nowDate.getTime() - loveDate.getTime();
+  const passedDay = Math.round(passedTime/(24*60*60*1000));
+  return (
+  <Styled.Wrapper>
+    <Styled.MainTop>
+        <Styled.LoveDate>
+          <p>우리 사랑</p>
+          <p>{loveYear}.{loveMonth}.{loveDay}부터</p>
+          <p>{passedDay}일♥</p>
+        </Styled.LoveDate>
+
+        <Styled.MainImg>
+          <img src="images/main_img.jpg" alt="main" />
+        </Styled.MainImg>
+    </Styled.MainTop>
+
+    <Styled.Profile>
+      <div className="me">My profile
+        <div className="meImg">
+          <img src="" alt="" />
+          <FiCamera />
+        </div>
+        <div className="profileText"></div>
+      </div>
+      <div className="heartImg">
+        하트이미지
+      </div>
+      <div className="you">Your Profile
+        <div className="youImg">
+          <img src="" alt="" />
+          <FiCamera />
+        </div>
+        <div className="profileText"></div>
+      </div>
+    </Styled.Profile>
+  </Styled.Wrapper>
+  );
 };
 
 export default Main;
--- src/pages/Main/styled.jsx
+++ src/pages/Main/styled.jsx
@@ -1,1 +1,32 @@
 import styledComponents from "styled-components";
+
+export const Wrapper = styledComponents.div`
+    font-family: 'Gowun Dodum', sans-serif;
+    padding:30px;
+`
+export const MainTop = styledComponents.div`
+    border:1px solid #000;
+    display:flex;
+    align-items:center;
+    justify-content:space-around;
+`
+export const LoveDate = styledComponents.div`
+    width:300px;
+    height:300px;
+    text-align:center;
+`
+export const MainImg = styledComponents.div`
+    width:700px;
+    height:300px;
+    overflow:hidden;
+    img{
+        width:100%;
+    }
+`
+export const Profile = styledComponents.div`
+    width:100%;
+    border:1px solid #000;
+    margin-top:30px;
+    display:flex;
+    justify-content:center;
+`
