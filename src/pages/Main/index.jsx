63e9d11af7cbdea6cc6d2d09ba4660cf0afe58a7
juillet <salut.juilllet@gmail.com>
2022-05-08 16:34:49 +0900

    아이콘 설치

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,52 +1,9 @@
-import React from "react";
-import * as Styled from "./styled";
-import { FiCamera } from 'react-icons/fi';
+import React from 'react'
 
 const Main = () => {
-  const loveDate = new Date('2022-05-04');
-  const loveYear = loveDate.getFullYear();
-  const loveMonth = loveDate.getMonth();
-  const loveDay = loveDate.getDate();
-
-  const nowDate = new Date();
-
-  const passedTime = nowDate.getTime() - loveDate.getTime();
-  const passedDay = Math.round(passedTime/(24*60*60*1000));
   return (
-  <Styled.Wrapper>
-    <Styled.MainTop>
-        <Styled.LoveDate>
-          <p>우리 사랑</p>
-          <p>{loveYear}.{loveMonth}.{loveDay}부터</p>
-          <p>{passedDay}일♥</p>
-        </Styled.LoveDate>
+    <div>Main</div>
+  )
+}
 
-        <Styled.MainImg>
-          <img src="images/main_img.jpg" alt="main" />
-        </Styled.MainImg>
-    </Styled.MainTop>
-
-    <Styled.Profile>
-      <div className="me">My profile
-        <div className="meImg">
-          <img src="" alt="" />
-          <FiCamera />
-        </div>
-        <div className="profileText"></div>
-      </div>
-      <div className="heartImg">
-        하트이미지
-      </div>
-      <div className="you">Your Profile
-        <div className="youImg">
-          <img src="" alt="" />
-          <FiCamera />
-        </div>
-        <div className="profileText"></div>
-      </div>
-    </Styled.Profile>
-  </Styled.Wrapper>
-  );
-};
-
-export default Main;
+export default Main
