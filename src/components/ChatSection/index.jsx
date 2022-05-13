70b0e0586ff359d2ff1dbc023c71881929a6c216
libby0322 <98502076+libby0322@users.noreply.github.com>
2022-05-13 16:40:37 +0900

    채팅창 하단 바 추가 및 상단작업 시작
    채팅창 하단 바 추가 및 상단작업 시작

--- src/components/ChatSection/index.jsx
+++ src/components/ChatSection/index.jsx
@@ -1,10 +1,25 @@
+import { type } from "@testing-library/user-event/dist/type";
 import * as Styled from "./styled";
 
+
 const ChatSection = () => {
+ 
   return (
     <Styled.Container >
+      {/* 탑바 */}
+      <Styled.Topbar>
+        <Styled.TopFrofileMe />
+        <Styled.TopFrofileYou />
+      </Styled.Topbar>
+
+        {/* 하단바 */}
         <Styled.Rowbar>
-        <Styled.chatBox />
+        <Styled.chatBox>
+        <Styled.chatText 
+        maxLength={400} 
+        autoFocus
+        placeholder="채팅을 시작해보세요!"/>
+        </Styled.chatBox>
         <Styled.send> send </Styled.send>
         </Styled.Rowbar>
     </Styled.Container >
--- src/components/ChatSection/styled.jsx
+++ src/components/ChatSection/styled.jsx
@@ -1,27 +1,79 @@
 import styled from "styled-components";
+// import heart from "../../../public/images/main/heart.jpg";
 
 export const Container =styled.div`
 width: 100%;
-height: 570px;
-background: #fe6fd9;
+height: 785px;
+background: #f5c7e9;
 `
-export const chatBox = styled.div`
+
+export const Topbar = styled.div`
 display: flex;
 justify-content: flex-start;
-margin-left: 5px;
-width: 1200px;
-bottom:50px;
-height: 50px;
-background: #fff;
+width: 100%;
+height: 80px;
+background: #f5c7e9;
+border: 2px solid #ddd;
 border-radius: 5px;
 `
-export const send = styled.button`
+export const TopFrofileMe = styled.div`
 display: flex;
-justify-content: flex-end;
-margin-left: 10px;
-margin-bottom: 100px;
+background-image: url("images/main/meimg.jpg");
+width: 50px;
+height: 50px;
+margin-top: 10px;
+margin-left: 30px;
+border-radius: 50%;
+`
+// export const He = styled(heart).img;
+
+export const TopFrofileYou = styled.div`
+display: flex;
+background-image: url("images/main/youimg.jpg");
+width: 50px;
+height: 50px;
+width: 50px;
+height: 50px;
+margin-top: 10px;
+margin-left: 50px;
+border-radius: 50%;
+`
+
+
+export const Rowbar = styled.div`
+display: flex;
+justify-content: space-between;
+background: #fe6fd9;
+width: 100%;
+height: 100%;
+
+`
+
+export const chatBox = styled.div`
+margin-top: 720px;
+margin-left: 280px;
+width: 1250px;
+height: 50px;
+background: #fff;
+border: 1px solid #fff;
+border-radius: 10px;
+`
+
+export const send = styled.button`
+margin-top: 720px;
+margin-right: 60px;
+width: 80px;
+height: 50px;
 border: 1px solid #fff;
 background-color: yellow;
 padding: 15px 15px;
-
+border-radius: 5px;
+`
+export const chatText = styled.textarea`
+border: 1px solid #fff;
+border-radius: 5px;
+width: 100%;
+height: 100%;
+text-align: center;
+font-size: 15px;
 `
